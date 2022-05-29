ab94752b4930690fecd3ce11529e46577eed2dbd
vara35 <kadiyalavaraprasad35@gmail.com>
2022-05-29 14:54:06 +0000

    push setTimer code

---
+++ src/components/Clock/index.js
@@ -0,0 +1,25 @@
+import {useState, useEffect} from 'react'
+import {ClockContainer, Heading, ClockImage, Time} from './styledComponents'
+
+const Clock = () => {
+  const [date, setDate] = useState(new Date())
+  useEffect(() => {
+    const setTimer = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    return () => {
+      clearInterval(setTimer)
+    }
+  }, [])
+  return (
+    <ClockContainer>
+      <Heading>Clock</Heading>
+      <ClockImage
+        src="https://assets.ccbp.in/frontend/hooks/clock-img.png"
+        alt="clock"
+      />
+      <Time> {date.toLocaleTimeString()} </Time>
+    </ClockContainer>
+  )
+}
+export default Clock
